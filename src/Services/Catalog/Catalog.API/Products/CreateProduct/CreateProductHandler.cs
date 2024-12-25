708441992b7c3a5ec5edda66e3321a3a1d2a248b
Bilal Ahmad <ba269953@gmail.com>
2024-12-25 22:23:45 +0500

    Added CQRS and MediaTR

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,string Description,decimal Price,string ImageFile,List<string> Category) : ICommand<CreateProductResponse>;
+
+    public record CreateProductResult(Guid Id);
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
