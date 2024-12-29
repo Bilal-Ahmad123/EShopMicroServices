b4562e6aaf46ec7c05da98dbd07364c56253c7de
Bilal Ahmad <ba269953@gmail.com>
2024-12-29 23:08:06 +0500

    Set up postgres docker container

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,26 @@
 ﻿using BuildingBlocks.CQRS;
-using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name,string Description,decimal Price,string ImageFile,List<string> Category) : ICommand<CreateProductResponse>;
+    public record CreateProductCommand(string Name,string Description,decimal Price,string ImageFile,List<string> Category) : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+    public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Catalog.API.Models.Products
+            {
+                id = Guid.NewGuid(),
+                Name = command.Name,
+                Description = command.Description,
+                Price = command.Price,
+                ImageFile = command.ImageFile,
+                Category = command.Category
+            };
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new CreateProductResult(product.id);
         }
     }
 }
