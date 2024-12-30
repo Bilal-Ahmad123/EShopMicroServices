468122c81f3b0db7d52d87ccf4f5f398f07a3a52
Bilal Ahmad <ba269953@gmail.com>
2024-12-30 23:50:30 +0500

    Added GetProductsQuery Handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductResult(IEnumerable<Catalog.API.Models.Products> products);
+    public record GetProductQuery():IQuery<GetProductResult>;
+    public class GetProductsQueryHandler(IDocumentSession session,ILogger<GetProductsQueryHandler> logger):IQueryHandler<GetProductQuery, GetProductResult>
+    {
+        public async Task<GetProductResult> Handle(GetProductQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductQueryHandler.handle called the query");
+            var products = await session.Query<Catalog.API.Models.Products>().ToListAsync(cancellationToken);
+            return new GetProductResult(products);
+        }
+    }
+}
