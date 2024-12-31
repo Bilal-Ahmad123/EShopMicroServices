74ed1b1245747078ddc52608f10c580d253b5b86
Bilal Ahmad <ba269953@gmail.com>
2025-01-01 00:00:46 +0500

    Added Delete Command

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,14 +1,17 @@
 ﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 
 namespace Catalog.API.Products.DeleteProduct
 {
     public record DeleteProductCommand(Guid Id):ICommand<DeleteProductResult>;
-    public record DeleteProductResult(Guid Id);
+    public record DeleteProductResult(bool IsSuccess);
     public class DeleteProductHandler(IDocumentSession session) : ICommandHandler<DeleteProductCommand, DeleteProductResult>
     {
-        public Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            session.Delete<Product>(request.Id);
+            await session.SaveChangesAsync(cancellationToken);
+            return new DeleteProductResult(true);
         }
     }
 }
