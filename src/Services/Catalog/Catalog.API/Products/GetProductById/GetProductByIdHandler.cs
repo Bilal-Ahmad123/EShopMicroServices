fb63ba138dbae698db55f9c5b1b5ffba784d4da1
Bilal Ahmad <ba269953@gmail.com>
2025-01-03 00:08:35 +0500

    Added Custom Exception Handler

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -11,7 +11,7 @@ namespace Catalog.API.Products.GetProductById
         public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
         {
             var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
-            if (product is null) throw new ProductNotFoundException();
+            if (product is null) throw new ProductNotFoundException(query.Id);
             return new GetProductByIdResult(product);
         }
     }
