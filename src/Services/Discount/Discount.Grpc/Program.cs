047b2a55079604cf0db5cf8ddafe7b240a5f5df4
Bilal Ahmad <ba269953@gmail.com>
2025-01-06 23:55:20 +0500

    --

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,20 @@
 
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database")!);
+});
 
 var app = builder.Build();
+app.MapGrpcService<DiscountService>();
+
 
 // Configure the HTTP request pipeline.
 
