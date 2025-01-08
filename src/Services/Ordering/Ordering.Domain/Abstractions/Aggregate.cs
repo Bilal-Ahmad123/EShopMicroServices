7e69f39ef0a9c6455e2242a1a789f41f2602012f
Bilal Ahmad <ba269953@gmail.com>
2025-01-08 23:43:36 +0500

    Added Domain Events

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId>:Entity<TId>, IAggregate<TId>
+    {
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents;
+        private List<IDomainEvent> _domainEvents = new();
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            var events = _domainEvents.ToArray();
+            _domainEvents.Clear();
+            return events;
+        }
+
+        protected void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+    }
+    {
+    }
+}
