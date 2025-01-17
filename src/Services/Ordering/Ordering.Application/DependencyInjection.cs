9f6c43fa5eef110334a3e7c3f50c184aa4b7179e
Bilal Ahmad <ba269953@gmail.com>
2025-01-17 23:52:41 +0500

    Complete RabbitMQ impl

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,24 +1,26 @@
 ﻿using BuildingBlocks.Behaviors;
-using System.Reflection;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
 using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.FeatureManagement;
+using System.Reflection;
 
-namespace Ordering.Application
+namespace Ordering.Application;
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplicationServices
+        (this IServiceCollection services, IConfiguration configuration)
     {
-        public static IServiceCollection AddApplicationServices
-                    (this IServiceCollection services, IConfiguration configuration)
+        services.AddMediatR(config =>
         {
-            services.AddMediatR(config =>
-            {
-                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-            });
-            services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
-            return services;
-        }
+            config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddFeatureManagement();
+        services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
+
+        return services;
     }
 }
